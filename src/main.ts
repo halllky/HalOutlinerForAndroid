c594d44b4f6d8fe089dd4228953e81db3965d74c
halky <yuyami.stars@gmail.com>
2019-10-27 07:16:44 +0900

    モバイル端末でDrag&Drop有効化

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
+// モバイル端末でドラッグドロップを有効にする
+import { polyfill } from 'mobile-drag-drop';
+import { scrollBehaviourDragImageTranslateOverride } from 'mobile-drag-drop/scroll-behaviour';
+import 'mobile-drag-drop/default.css';
+polyfill({ dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride });
+
 new Vue({
   store,
   render: (h) => h(App),
